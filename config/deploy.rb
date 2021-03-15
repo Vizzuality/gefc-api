c46aaea9e1c1fd26fb1309f6398698a83440f09b
CV-Gate <miguel.mendoza@vizzuality.com>
2021-03-15 16:37:54 +0100

    adds cap rbenv config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,8 @@ set :deploy_to, '/var/www/gefc_api'
 append :linked_files, '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/downloads', 'vendor/bundle'
 set :keep_releases, 3
+set :rbenv_type, :user
+set :rbenv_ruby, '3.0.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
