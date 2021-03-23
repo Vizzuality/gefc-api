b1fd41dcf7bd1eb494776f149e9f8b0065026450
develaper <ibrahim.lachguer@gmail.com>
2021-03-23 16:55:26 +0100

    Adds subgroups and relationship with groups

---
+++ app/models/subgroup.rb
@@ -0,0 +1,3 @@
+class Subgroup < ApplicationRecord
+    belongs_to :group
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_153651) do
+ActiveRecord::Schema.define(version: 2021_03_23_153846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_03_23_153651) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subgroups", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "group_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_subgroups_on_group_id"
   end
 
 end
