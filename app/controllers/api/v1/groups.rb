91f10b415b057c54d71edd992a7bbe61462e42da
Ibrah <ibrahim.lachguer@gmail.com>
2021-06-10 07:25:21 +0200

    Adds nested routes for subgroups and indicators

--- app/controllers/api/v1/groups.rb
+++ app/controllers/api/v1/groups.rb
@@ -17,6 +17,42 @@ module API
 				get ":id", root: "group" do
 					present Group.where(id: permitted_params[:id]).first!, with: API::V1::Entities::Group
 				end
+
+				desc "Return a group subgroups"
+				params do
+					requires :id, type: String, desc: "ID of the group"
+				end
+				get ":id/subgroups" do
+					present Subgroup.where(group_id: permitted_params[:id]), with: API::V1::Entities::Subgroup
+				end
+
+				desc "Return a group subgroup by id"
+				params do
+					requires :id, type: String, desc: "ID of the group"
+					requires :subgroup_id, type: String, desc: "ID of the subgroup"
+				end
+				get ":id/subgroups/:subgroup_id" do
+					present Subgroup.find(permitted_params[:subgroup_id]), with: API::V1::Entities::Subgroup
+				end
+
+				desc "Return subgroup indicators"
+				params do
+					requires :id, type: String, desc: "ID of the group"
+					requires :subgroup_id, type: String, desc: "ID of the subgroup"
+				end
+				get ":id/subgroups/:subgroup_id/indicators" do
+					present Indicator.where(subgroup_id: permitted_params[:subgroup_id]), with: API::V1::Entities::FullIndicator
+				end
+				
+				desc "Return subgroup indicator by id"
+				params do
+					requires :id, type: String, desc: "ID of the group"
+					requires :subgroup_id, type: String, desc: "ID of the subgroup"
+					requires :indicator_id, type: String, desc: "ID of the indicator"
+				end
+				get ":id/subgroups/:subgroup_id/indicators/:indicator_id" do
+					present Indicator.find(permitted_params[:indicator_id]), with: API::V1::Entities::FullIndicator
+				end
 			end
 		end
 	end
