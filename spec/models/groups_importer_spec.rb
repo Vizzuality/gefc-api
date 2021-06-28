c2ed25abd820015dd7b067fe5714bf531dda31ed
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-06-28 13:23:22 +0200

    Translated columns importer changes

--- spec/models/groups_importer_spec.rb
+++ spec/models/groups_importer_spec.rb
@@ -7,14 +7,23 @@ RSpec.describe GroupsImporter, type: :model do
   #
   it "creates exact number of records for a valid csv" do
     arr_of_rows = CSV.read(file_path, headers: true)
-    expect{ GroupsImporter.new.import_from_csv(file_path) }.to change{ Record.all.count }.by(arr_of_rows.count)    
+
+    expect{ GroupsImporter.new.import_from_csv(file_path) }.to change{ Record.all.count }.by(arr_of_rows.count)
   end
+
+  it "creates exact number of groups for a valid csv" do
+    expect{ GroupsImporter.new.import_from_csv(file_path) }.to change{ Group.all.count }.by(1)
+  end
+
   # Atention! we are using a csv with 9 rows with line set to true
   #
-  it "stablish a relationship between record and the assigned widgets" do
+  it "establish a relationship between record and the assigned widgets" do
     arr_of_rows = CSV.read(file_path, headers: true)
-    line = create(:widget, name: 'line')
-    expect{ GroupsImporter.new.import_from_csv(file_path) }.to change{ RecordWidget.where(widget: line).count }.by(arr_of_rows.count)
+
+    expect { GroupsImporter.new.import_from_csv(file_path) }.to change {
+      line = Widget.find_by_name('line')
+      RecordWidget.where(widget: line).count
+    }.by(arr_of_rows.count)
   end
 
   it "does not stablish a relationship between record and non assigned widgets" do
@@ -23,7 +32,7 @@ RSpec.describe GroupsImporter, type: :model do
     pie = create(:widget, name: 'pie')
     choropleth = create(:widget, name: 'choropleth')
     non_assigned_widgets = [pie, chart, choropleth]
-    
-    expect{ GroupsImporter.new.import_from_csv(file_path) }.to_not change{ RecordWidget.where(widget: non_assigned_widgets).count }
+
+    expect { GroupsImporter.new.import_from_csv(file_path) }.to_not change{ RecordWidget.where(widget: non_assigned_widgets).count }
   end
 end
