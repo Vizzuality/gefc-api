ab15a77eae2a27c6d593c4fe20183028978c43d2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-06-29 13:35:53 +0200

    Enabled postgis extension

---
+++ db/migrate/20210629113106_enable_postgis.rb
@@ -0,0 +1,24 @@
+class EnablePostgis < ActiveRecord::Migration[6.1]
+  def change
+    reversible do |dir|
+      dir.up do
+        if Rails.env.development? || Rails.env.test? || version_13plus?
+          # only CREATEDB privilege required in 13+ rather than SUPERUSER
+          execute 'CREATE EXTENSION IF NOT EXISTS "postgis";'
+        end
+      end
+      dir.down do
+        if Rails.env.development? || Rails.env.test? || version_13plus?
+          execute 'DROP EXTENSION IF EXISTS "postgis";'
+        end
+      end
+    end
+  end
+
+  def version_13plus?
+    # e.g. 11.5
+    version = ActiveRecord::Base.connection.select_value('show server_version')
+    major = version.split('.').first
+    major.to_i >= 13
+  end
+end
