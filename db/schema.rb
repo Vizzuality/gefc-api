d8683d24be640f29a66420bf8c8e5cce975ef3ea
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-07-01 09:50:57 +0200

    Fixed Active storage record id type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_082212) do
+ActiveRecord::Schema.define(version: 2021_07_01_071614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 2021_06_30_082212) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.uuid "record_id", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "active_storage_attachments_uniqueness_idx", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
