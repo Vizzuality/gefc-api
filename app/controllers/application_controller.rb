0595b200e42cde3b1490e4874b4575d529ec265d
develaper <ibrahim.lachguer@gmail.com>
2021-07-06 17:14:50 +0200

    Deleted useless devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
-class ApplicationController < ActionController::Base
-    # skip_before_action :verify_authenticity_token
-    # protect_from_forgery with: :null_session
-
-    # respond_to :json  
+class ApplicationController < ActionController::Base 
 end
--- app/controllers/authentication_controller.rb
+++
@@ -1,7 +0,0 @@
-# class AuthenticationController < Devise::SessionsController
-#   protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
-# 	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-#   skip_before_action :authenticate_user!, only: [:create]
-    
-#   respond_to :json
-# end
