252f0a4fac0609be464a23bb34e76fb2b0a34a3e
develaper <ibrahim.lachguer@gmail.com>
2021-07-06 13:11:37 +0200

    Added domain

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'ec2-18-140-53-211.ap-southeast-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'gefc.dev-vizzuality.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
 set :branch, 'develop'
