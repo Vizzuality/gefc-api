6d8ebfe8922d26ce1bf01d6c2881acd711832792
develaper <ibrahim.lachguer@gmail.com>
2021-08-24 17:35:12 +0200

    Adds endpoint to request recovery link

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    username { Faker::Name.name }
+    password { "password" }
+    password_confirmation { "password" }    
   end
 end
