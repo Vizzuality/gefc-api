66d20bd51c44bd90be47d225e662b4557368200d
develaper <ibrahim.lachguer@gmail.com>
2021-11-10 10:46:05 +0100

    Added filter records by visualization

--- app/controllers/api/v1/groups.rb
+++ app/controllers/api/v1/groups.rb
@@ -89,13 +89,14 @@ module API
 					optional :year, type: Integer, desc: "Year"
 					optional :start_year, type: Integer, desc: "Start year"
 					optional :end_year, type: Integer, desc: "End year"
+					optional :visualization, type: String, desc: "Name of the widget"
 					use :pagination
 				end
 				get ":id/subgroups/:subgroup_id/indicators/:indicator_id/records" do
           group = Group.find_by_id_or_slug!(permitted_params[:id])
           subgroup = Subgroup.find_by_id_or_slug!(permitted_params[:subgroup_id], group_id: group.id)
           indicator = Indicator.find_by_id_or_slug!(permitted_params[:indicator_id], {subgroup_id: subgroup.id}, [])
-					filter = FilterIndicatorRecords.new(indicator, params.slice(:category_1, :scenario, :region, :unit, :year, :start_year, :end_year))
+					filter = FilterIndicatorRecords.new(indicator, params.slice(:category_1, :scenario, :region, :unit, :year, :start_year, :end_year, :visualization))
 					records = filter.call.includes(:widgets, :unit, :region, :scenario).order(year: :desc)
 					#no need to order by year if there is only one year
 					present records.page(params[:page]).per(params[:per_page]), with: API::V1::Entities::Record
