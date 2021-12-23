f09b2b2e449c6b726df7f861c8f2ddfbb747a61e
develaper <ibrahim.lachguer@gmail.com>
2021-12-23 13:04:13 +0100

    Added swagger_url

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,3 @@
-GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
 GrapeSwaggerRails.options.before_action do
-	GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
+	GrapeSwaggerRails.options.app_url = ENV['SWAGGER_URL']
 end
