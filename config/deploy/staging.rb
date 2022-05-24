2a30a176d09ec6cc09e49b7393865a2a4d72b904
develaper <ibrahim.lachguer@gmail.com>
2022-05-24 13:35:31 +0200

    Changed log_level

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 server 'gefc.dev-vizzuality.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
-set :branch, 'develop'
+set :branch, 'feature/importer_with_sideqik'
