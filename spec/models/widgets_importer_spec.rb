96cd6ef814b57c5a5b36b6abc5226fac562ee5e9
Tiago Garcia <tiagojsag@gmail.com>
2022-09-07 15:42:12 +0200

    Improvements to the data import process

--- spec/models/widgets_importer_spec.rb
+++ spec/models/widgets_importer_spec.rb
@@ -4,14 +4,30 @@ require "csv"
 RSpec.describe WidgetsImporter, type: :model do
   let(:file_path) { "spec/files/local_json/widgets_import_test.json" }
 
+  before(:all) do
+    group = Group.create({name_en: "Socioeconomic"})
+    subgroup = Subgroup.create({name_en: "Agriculture", group: group})
+    Indicator.create({name_en: "Total Power of Agricultural Machinery", subgroup: subgroup}).save!
+    Indicator.create({name_en: "Farm Machinery", subgroup: subgroup}).save!
+    Indicator.create({name_en: "Irrigated Area of Cultivated Land", subgroup: subgroup}).save!
+    Indicator.create({name_en: "Consumption of Chemical Fertilizers", subgroup: subgroup}).save!
+    Indicator.create({name_en: "Irrigated Areas", subgroup: subgroup}).save!
+  end
+
+  after(:all) do
+    Indicator.delete_all
+    Subgroup.delete_all
+    Group.delete_all
+  end
+
   # Happy path with a valid JSON.
   #
-  # Atention! we are using a json with 5 elements
+  # Attention! we are using a json with 5 elements
   #
   it "creates exact number of indicators for a valid json" do
     file = File.read(file_path)
     data_hash = JSON.parse(file)
-    expect { WidgetsImporter.new.import_from_file(file_path) }.to change { Indicator.all.count }.by(data_hash.count)
+    expect { WidgetsImporter.new.import_from_file(file_path) }.to change { IndicatorWidget.all.count }.by((data_hash.map { |e| e["widget"].count }).sum)
   end
   it "sets Indicator.by_default for each indicator" do
     file = File.read(file_path)
