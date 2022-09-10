fa8e935cbb397ed4d14770919d79875a0fb119f5
Tiago Garcia <tiagojsag@gmail.com>
2022-09-10 19:09:49 +0200

    Fix nodejs version on capistrano deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,3 +12,8 @@ set :rbenv_ruby, "3.0.0"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 set :rbenv_roles, :all
+
+set :default_env, {
+  PATH: '$HOME/.nvm/versions/node/v12.16.3/bin/:$PATH',
+  NODE_ENVIRONMENT: 'production'
+}
