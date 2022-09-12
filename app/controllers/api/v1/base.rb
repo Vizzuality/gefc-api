7d214d51115cf35f7173e7e5df2fbcb9ff96b550
Tiago Garcia <tiagojsag@gmail.com>
2022-09-12 11:30:49 +0200

    Fix swagger paths

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -10,11 +10,10 @@ module API
       mount API::V1::Downloads
       mount API::V1::Health
 
-      # add_swagger_documentation
       add_swagger_documentation(
         api_version: "v1",
         hide_documentation_path: true,
-        mount_path: "#{Rails.configuration.relative_url_root}/api/v1/swagger_doc",
+        mount_path: "/swagger_doc",
         hide_format: true
       )
     end
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 GrapeSwaggerRails.options.before_action do
-  GrapeSwaggerRails.options.url = "#{Rails.configuration.relative_url_root}/api/v1/swagger_doc"
+  GrapeSwaggerRails.options.url = "#{Rails.configuration.relative_url_root}/swagger_doc"
   GrapeSwaggerRails.options.app_url = ENV["API_BASE_URL"] || ""
 end
