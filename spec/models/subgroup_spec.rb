d90e34dfcebed97aeeb157707fcd638bf40b65c3
Tiago Garcia <tiagojsag@gmail.com>
2022-11-10 06:17:47 +0100

    Fix tests

--- spec/models/subgroup_spec.rb
+++ spec/models/subgroup_spec.rb
@@ -12,11 +12,20 @@ RSpec.describe Subgroup, type: :model do
 
     expect(subgroup).to_not be_valid
   end
-  it "has a uniq name" do
-    subgroup = create(:subgroup)
-    new_subgroup = build(:subgroup, name_en: subgroup.name_en)
+  it "has a uniq name within the group" do
+    group = create(:group, name_en: "group 1")
+    subgroup = create(:subgroup, group: group)
+    new_subgroup = build(:subgroup, name_en: subgroup.name_en, group: group)
 
     expect(new_subgroup).to_not be_valid
+  end
+  it "can have duplicate name across different groups" do
+    group_one = create(:group, name_en: "group 1")
+    group_two = create(:group, name_en: "group 2")
+    subgroup = create(:subgroup, group: group_one)
+    new_subgroup = build(:subgroup, name_en: subgroup.name_en, group: group_two)
+
+    expect(new_subgroup).to be_valid
   end
   it "has a valid slug" do
     subgroup = create(:subgroup)
