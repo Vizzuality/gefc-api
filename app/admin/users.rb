34459e90a3157ec8c19a58ec47c3c822b0e5c426
Tiago Garcia <tiagojsag@gmail.com>
2022-11-24 13:21:23 +0100

    Update active admin user model config

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,48 +1,57 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation, :role
+  controller do
 
-  show :title => :name do
+    def update
+      model = :user
+
+      if params[model][:password].blank?
+        %w(password password_confirmation).each { |p| params[model].delete(p) }
+      end
+
+      super
+    end
+  end
+
+  permit_params :email, :password, :password_confirmation, :role, :title, :name, :organization
+
+  show title: :name do
     attributes_table do
-      row :title
       row :name
       row :email
       row :organization
-      row :created_at
+      row :title
       row :role
+      row :created_at
     end
   end
 
   index do
     selectable_column
-    id_column
-    column :title
+    column "Email address" do |user|
+      link_to user.email, admin_user_path(user)
+    end
     column :name
-    column :email
     column :organization
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :title
     column :role
+    column :created_at
     actions
   end
 
-  filter :title
   filter :name
   filter :email
   filter :organization
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :title
   filter :role
 
   form do |f|
     f.inputs do
-      f.input :title
       f.input :name
       f.input :email
       f.input :organization
+      f.input :title
+      f.input :role, as: :select, collection: User.roles.keys
       f.input :password
-      f.select :role, User.roles.keys, {}
     end
     f.actions
   end
